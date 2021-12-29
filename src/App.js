653cc9f99c9b4e22ba6bbbbb4310bf9445316ae7
AnwarHossainSR <mahedianwar@gmail.com>
2021-12-29 23:09:27 +0600

    react-swipeable

--- src/App.js
+++ src/App.js
@@ -1,34 +1,27 @@
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
-//import { SwipeEventListener } from "swipe-event-listener";
+import { useSwipeable } from "react-swipeable";
 
 const App = () => {
   const [state, setstate] = useState(false);
-  const ulRef = useRef();
-  // const { swipeArea } = SwipeEventListener({
-  //   swipeArea: document.querySelector("header"),
-  // });
-  // swipeArea.addEventListener("swipeRight", () => {
-  //   console.log("swipe right");
-  //   setstate(true);
-  // });
-  const handle = () => {
-    console.log(ulRef);
-    setstate(true);
-  };
+  const handlers = useSwipeable({
+    onSwipedRight: () => {
+      setstate(true);
+      console.log("swiped");
+    },
+  });
 
   return (
     <header className="container">
       <img src="https://cdn.logo.com/hotlink-ok/logo-social.png" alt="" />
-      <nav id="navbar">
-        <ul
-          draggable={true}
-          className={state ? "ul" : ""}
-          ref={ulRef}
-          onDrag={handle}
-        >
+      <nav {...handlers}>
+        <ul className={state ? "ul" : ""}>
           <li>Home</li>
           <li>About</li>
+        </ul>
+        <ul className={state ? "ul2" : "none"}>
+          <li>Home-2</li>
+          <li>About-2</li>
         </ul>
       </nav>
     </header>
