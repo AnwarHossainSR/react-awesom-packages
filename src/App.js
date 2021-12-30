684f1d4e03182cb20c0083b67ed7249827622267
AnwarHossainSR <mahedianwar@gmail.com>
2021-12-30 20:41:40 +0600

    react-bootstrap package

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import { Button, DatePicker } from "antd";
-import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
   return (
-    <>
-      <Button type="primary">PRESS ME</Button>
-      <DatePicker placeholder="select date" />
-    </>
+    <div>
+      <button className="btn btn-primary">Click</button>
+    </div>
   );
 };
 
